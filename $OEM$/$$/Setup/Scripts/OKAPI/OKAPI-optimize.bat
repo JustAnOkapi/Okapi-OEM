ba876d3800c06a5e822db491976e1dbde473bbca
JustAnOkapi <JustAnOkapi@gmail.com>
2021-02-01 01:47:11 -0600

    install latest Java 8 update 281 for 32 and 64 bit

--- $OEM$/$$/Setup/Scripts/OKAPI/OKAPI-optimize.bat
+++ $OEM$/$$/Setup/Scripts/OKAPI/OKAPI-optimize.bat
@@ -10,6 +10,9 @@ rem [INFO] run Windows10Debloater
 powershell.exe -ExecutionPolicy Unrestricted -Command ". '%cdrom%\post\scripts\Windows10SysPrepDebloater.ps1' -Debloat -SysPrep"
 rem [INFO] install all latest Microsoft Visual C++ Redistributable Runtimes
 %cdrom%\post\dependencies\VisualCppRedist_AIO_x86_x64.exe /aiA /gm2
+rem [INFO] install latest Java 8 update 281 for 32 and 64 bit
+start /wait %cdrom%\post\jre-8u281-windows-i586.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
+start /wait %cdrom%\post\jre-8u281-windows-x64.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
 
 
 PAUSE
