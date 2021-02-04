4ce4209b71c2df89b45dfba3fbf026f33e4f9029
JustAnOkapi <JustAnOkapi@gmail.com>
2021-02-04 03:04:36 -0600

    install only proper java file

--- $OEM$/$$/Setup/Scripts/OKAPI/OKAPI-optimize.bat
+++ $OEM$/$$/Setup/Scripts/OKAPI/OKAPI-optimize.bat
@@ -11,8 +11,9 @@ powershell.exe -ExecutionPolicy Unrestricted -Command ". '%cdrom%\post\scripts\W
 rem [INFO] install all latest Microsoft Visual C++ Redistributable Runtimes
 %cdrom%\post\dependencies\VisualCppRedist_AIO_x86_x64.exe /aiA /gm2
 rem [INFO] install latest Java 8 update 281 for 32 and 64 bit
-start /wait %cdrom%\post\jre-8u281-windows-i586.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
-start /wait %cdrom%\post\jre-8u281-windows-x64.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
+If %PROCESSOR_ARCHITECTURE%==x86 start /wait %cdrom%\post\jre-8u281-windows-i586.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
+If %PROCESSOR_ARCHITECTURE%==IA64 start /wait %cdrom%\post\jre-8u281-windows-x64.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
+If %PROCESSOR_ARCHITECTURE%==AMD64 start /wait %cdrom%\post\jre-8u281-windows-x64.exe /s AUTO_UPDATE=0 REBOOT=Suppress REMOVEOUTOFDATEJRES=1
 
 
 PAUSE
